3e73e3ddbbdc62d9f40d3a72243859dc5bddc6c4
Jude Fanner <judeefanner@gmail.com>
2025-12-03 17:09:50 +0200

    initial commit

---
+++ serve.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Simple HTTP server for the Oklab color explorer"""
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map['.csv'] = 'text/csv'
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Server running at http://localhost:{PORT}/")
+    print(f"Open http://localhost:{PORT}/index.html in your browser")
+    print("Press Ctrl+C to stop the server")
+    httpd.serve_forever()
